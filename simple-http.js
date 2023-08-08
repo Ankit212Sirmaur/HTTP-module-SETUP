9797dd480bb662486e9df9bfd790264e3e7abceb
AnkitKumar <ankit17110747@yahoo.com>
2023-08-08 17:11:14 +0530

    query-params URL are added

--- simple-http.js
+++ simple-http.js
@@ -1,10 +1,11 @@
 const http = require("http");
 const fs = require("fs");
-const { isUtf8 } = require("buffer");
+const url = require('url');
 
 const port = 2122;
 const server = http.createServer((req, res) => {
   const log = `${Date.now()}: ${req.url} new req received\n`;
+  const myUrl = url.parse(req.url, true);
   fs.appendFile("log.txt",log, (err, data) => {
     switch (req.url) {
       case "/":
@@ -13,6 +14,9 @@ const server = http.createServer((req, res) => {
       case "/about":
         res.end("about-page");
         break;
+        case '/search':
+          const search = myUrl.query.search_query;
+          res.end('here are your results for ' + search);
       default:
         res.end(JSON.stringify({ msg: 'req not found' }));
     }
